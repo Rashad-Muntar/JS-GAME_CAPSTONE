c0f8bcc6356a0630a69dcd1357da1d25724446ba
Rashad-Muntar <muntartoure@gmail.com>
2021-07-11 00:43:00 +0000

    add index and model files

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import Phaser from 'phaser';
+import config from './Config/config';
+import GameScene from './Scenes/GameScene';
+import BootScene from './Scenes/BootScene';
+import PreloaderScene from './Scenes/PreloaderScene';
+import TitleScene from './Scenes/TitleScene';
+import OptionsScene from './Scenes/OptionsScene';
+import CreditsScene from './Scenes/CreditsScene';
+import GameOverScene from './Scenes/GameOverScene';
+import FormScene from './Scenes/formScene';
+import LeaderBoard from './Scenes/LeaderBoard';
+import Model from './Model';
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    const model = new Model();
+    this.globals = { model, bgMusic: null };
+    this.scene.add('FormScene', FormScene);
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
+    this.scene.add('Game', GameScene);
+    this.scene.add('GameOver', GameOverScene);
+    this.scene.add('LeaderBoard', LeaderBoard);
+    this.scene.start('Boot');
+  }
+}
+
+window.game = new Game();
